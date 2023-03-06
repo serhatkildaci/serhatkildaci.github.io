256caa653f99babeb8181059d551bc060fc7e6ca
Serhat Kıldacı <96617749+serhatkildaci@users.noreply.github.com>
2023-03-06 17:35:38 +0300

    Bug fixes for miliseconds

--- stopwatchscript.js
+++ stopwatchscript.js
@@ -1,6 +1,6 @@
-const hours = 0;
-const minutes = 0;
 let seconds = 0;
+let minutes = 0;
+let hours = 0;
 let milliseconds = 0;
 
 let displaySeconds = 0;
@@ -12,51 +12,85 @@ let interval = null;
 
 let status = "stopped";
 
-function stopWatch() {
-  milliseconds++;
+function stopWatch(){
 
-  if (milliseconds / 1000 === 1) {
-    milliseconds = 0;
-    seconds++;
+    milliseconds++;
 
-    if (seconds / 60 === 1) {
-      seconds = 0;
-      minutes++;
+    if (milliseconds === 100){
+        milliseconds = 0;
+        seconds++;
 
-      if (minutes / 60 === 1) {
-        minutes = 0;
-        hours++;
-      }
+        if(seconds === 60){
+            seconds = 0;
+            minutes++;
+
+            if(minutes === 60){
+                minutes = 0;
+                hours++;
+            }
+        }
     }
-  }
 
-  displayMilliseconds = milliseconds < 10 ? "00" + milliseconds : milliseconds < 100 ? "0" + milliseconds : milliseconds;
-  displaySeconds = seconds < 10 ? "0" + seconds : seconds;
-  displayMinutes = minutes < 10 ? "0" + minutes : minutes;
-  displayHours = hours < 10 ? "0" + hours : hours;
+    if(milliseconds < 10){
+        displayMilliseconds = "0" + milliseconds.toString();
+    }
+    else if(milliseconds < 100){
+        displayMilliseconds = milliseconds.toString();
+    }
+    else{
+        displayMilliseconds = milliseconds.toString();
+    }
 
-  document.getElementById("display").innerHTML = `${displayHours}:${displayMinutes}:${displaySeconds}.${displayMilliseconds}`;
+    if(seconds < 10){
+        displaySeconds = "0" + seconds.toString();
+    }
+    else{
+        displaySeconds = seconds.toString();
+    }
+
+    if(minutes < 10){
+        displayMinutes = "0" + minutes.toString();
+    }
+    else{
+        displayMinutes = minutes.toString();
+    }
+
+    if(hours < 10){
+        displayHours = "0" + hours.toString();
+    }
+    else{
+        displayHours = hours.toString();
+    }    
+
+    document.getElementById("display").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds + ":" + displayMilliseconds;
 }
 
-function startStop() {
-  if (status === "stopped") {
-    // Start the stopwatch by calling the setInterval() function
-    interval = window.setInterval(stopWatch, 1);
-    document.getElementById("startStop").innerHTML = "Stop";
-    status = "started";
-  } else {
-    window.clearInterval(interval);
+
+function startStop(){
+
+    if(status === "stopped"){
+
+        interval = window.setInterval(stopWatch, 10);
+        document.getElementById("startStop").innerHTML = "Stop";
+        status = "started";
+
+    }
+    else{
+
+        window.clearInterval(interval);
+        document.getElementById("startStop").innerHTML = "Start"
+        status = "stopped";
+
+    }
+}
+
+function reset(){
+
+    window.clearInterval(interval)
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    milliseconds = 0;
+    document.getElementById("display").innerHTML = "00:00:00:00";
     document.getElementById("startStop").innerHTML = "Start";
-    status = "stopped";
-  }
-}
-
-function reset() {
-  window.clearInterval(interval);
-  seconds = 0;
-  minutes = 0;
-  hours = 0;
-  milliseconds = 0;
-  document.getElementById("display").innerHTML = "00:00:00.000";
-  document.getElementById("startStop").innerHTML = "Start";
 }
