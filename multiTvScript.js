4cf5cf6a2de06a98c5373d8887ad64a6dbadd780
Serhat Kıldacı <96617749+serhatkildaci@users.noreply.github.com>
2023-03-07 15:37:45 +0300

    Add files via upload

---
+++ multiTvScript.js
@@ -0,0 +1,22 @@
+
+/*WIP
+
+$(document).ready(function() {
+    $(".video").hide();
+    
+    $("#show-all").click(function() {
+      $(".video").show();
+    });
+    
+    $(".filter").click(function() {
+      var videoId = $(this).data("filter");
+      $(".video").each(function() {
+        if ($(this).data("iframe") == videoId) {
+          $(this).hide();
+        }
+      });
+    });
+  });
+
+
+ WIP*/
